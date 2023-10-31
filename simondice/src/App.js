1136fe5567d0f09df725d3efde30700c2855b2fd
4x3l15 <adramirez@escuelasproa.edu.ar>
2023-10-31 14:49:54 -0300

    avance

---
+++ simondice/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+import Botonera from "./componentes/Botonera";
+import Navbar from './componentes/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+        <Botonera/>
+    </div>
+  );
+}
+
+export default App;
---
+++ simondice/src/componentes/Botonera.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './Botonera.css';
+
+function Botonera() {
+
+  return (
+    <div className="parent">
+        <button></button>
+    </div>
+  );
+}
+
+export default Botonera;
