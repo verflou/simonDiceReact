e8d018ad950d67c7bb2b5dba1b43876f27fa4615
verflou <88504966+verflou@users.noreply.github.com>
2023-10-31 23:10:40 -0300

    nuevo avance
    avance

--- simondice/src/App.js
+++ simondice/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import Botonera from "./componentes/Botonera";
 import Navbar from './componentes/Navbar';
+import Footer from './componentes/Footer';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
         <Botonera/>
+        <Footer/>
     </div>
   );
 }
--- simondice/src/componentes/Botonera.js
+++ simondice/src/componentes/Botonera.js
@@ -1,14 +1,15 @@
-
 import React from 'react';
 import './Botonera.css';
 
-function Botonera() {
-
+function Botonera (){
   return (
-    <div className="parent">
-        <button></button>
+    <div className="simon">
+      <div className="boton rojo"></div>
+      <div className="boton azul"></div>
+      <div className="boton verde"></div>
+      <div className="boton amarillo"></div>
     </div>
   );
-}
+};
 
 export default Botonera;
